68bd45d05dcf12cc82eb383827ed36db22c5ef72
Tim Weidinger <timothy.weidinger@gmail.com>
2023-08-14 16:42:22 -0400

    dummy data set up for the job card and rendering some data to the screen

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 const dummyData = {
   name: 'Google',
-  salary: 200000,
+  salary: 2000000,
   position: 'Senior Software Developer',
   contact: {
     name: 'John Smith',
@@ -12,7 +12,7 @@ const dummyData = {
   },
   location: {
     headquarters: 'Mountain View, California',
-    type: 'Remote',
+    type: 'remote',
     address: 'Mountain View, California',
   },
   lastHeard: false,
@@ -20,16 +20,40 @@ const dummyData = {
 
 function JobCard(props) {
   const [dropdown, setDropdown] = useState({});
+
+  function formatSalary(salary) {
+    const newSalary = [];
+    const salArr = salary.split('').reverse();
+    for (let i = 0; i < salArr.length; i++) {
+      if (!(i % 3) && i !== 0) newSalary.push(',');
+      newSalary.push(salArr[i]);
+    }
+    newSalary.push('$');
+    return newSalary.reverse().join('');
+  }
+
   return (
     <div className='job-card'>
-      <div className='job-card-header' style={{display: 'flex'}}>
-        <p>Name: {dummyData.name}</p>
-        <p>Salary: {dummyData.salary}</p>
+      <div className='job-card-header'>
         <p>
-          Location: {dummyData.location.address} ({dummyData.location.type})
+          <b>Name:</b> {dummyData.name}
+        </p>
+        <p>
+          <b>Salary:</b> {formatSalary(dummyData.salary.toString())}
+        </p>
+        <p>
+          <b>Location:</b> {dummyData.location.address} (
+          {dummyData.location.type})
         </p>
       </div>
-      <div className='job-card-details'></div>
+      <div className='job-card-details'>
+        <h4>Position: </h4> <p>{dummyData.position}</p>
+        <h4>Date:</h4> <p>{new Date().toLocaleString()}</p>
+        <button>Update</button>
+        <h4>Contact:</h4> <p>{dummyData.contact.name}</p>
+        <h4>Last Heard:</h4> <p>{dummyData.lastHeard || 'no contact'}</p>
+        <button>Notes</button>
+      </div>
     </div>
   );
 }
