6e72657f5788f64e1d3e8128451e2e2bef2c65dd
Tim Weidinger <timothy.weidinger@gmail.com>
2023-08-15 15:49:53 -0400

    beginning of oauth stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
+import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
-  const [user, setUser] = useState({ username: 'Tim' });
-  const [loggedIn, setLoggedIn] = useState(false);
+  const {loginWithRedirect, logout, isAuthenticated, user} = useAuth0();
 
-  useEffect(() => {
-    user.hasOwnProperty('username') ? setLoggedIn(true) : setLoggedIn(false);
-  }, [user]);
   // some useEffect to grab the currently logged in user here from the backend authentication
   // set state of the logged in user - if the user exists, then conditionally render the dashboard below
-  function loginUser(obj) {
-    setUser(obj);
-    return;
-  }
 
+  console.log(isAuthenticated, 'IS AUTHENTICATED');
   return (
     <div className='app'>
-      <Navbar loginUser={(obj) => loginUser(obj)} loggedIn={loggedIn} />
-      {user.hasOwnProperty('username') ? (
+      <Navbar />
+      {isAuthenticated ? (
         <Dashboard />
       ) : (
         <div className='not-Logged-in'>Login to continue</div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App.jsx';
 import styles from './scss/App.scss';
+import {Auth0Provider} from '@auth0/auth0-react';
 
 // react render
 const root = createRoot(document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+console.log(domain, clientId);
 root.render(
-  <>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </>
+  </Auth0Provider>
 );
